db7db3ce5038c2a23b53c298ec002021733e2eef
Kavin Arasu <kavin@groupon.com>
2015-10-09 20:06:00 -0700

    Adding people count

--- tipCalculator/TipViewController.m
+++ tipCalculator/TipViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *peopleCountControl;
 
 @end
 
@@ -49,11 +50,18 @@
     [self updateValues];
 }
 
+- (IBAction)onPeopleCountChange:(UISegmentedControl *)sender {
+    [self updateValues];
+}
+
+
 - (void) updateValues {
     float billValue = [self.billTextField.text floatValue];
     NSArray *tipValues = @[@(0.15), @(0.2), @(0.25)];
-    float tipAmount = [tipValues[self.tipControl.selectedSegmentIndex] floatValue] * billValue;
-    float totalAmount = billValue + tipAmount;
+    int numOfPeopleIndex = (int) self.peopleCountControl.selectedSegmentIndex;
+    int numOfPeople = [self.peopleCountControl titleForSegmentAtIndex:numOfPeopleIndex].intValue;
+    float tipAmount = ([tipValues[self.tipControl.selectedSegmentIndex] floatValue] * billValue) / numOfPeople;
+    float totalAmount = billValue / numOfPeople + tipAmount;
     self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
 
     self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
