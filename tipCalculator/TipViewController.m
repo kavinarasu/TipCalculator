079ee8cd4d0d3d932b11ef54706ca45744797fda
Kavin Arasu <kavin@groupon.com>
2015-10-09 22:34:18 -0700

    Add colors

--- tipCalculator/TipViewController.m
+++ tipCalculator/TipViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TipViewController.h"
+#define Rgb2UIColor(r, g, b)  [UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:1.0]
+
 
 @interface TipViewController ()
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
@@ -18,6 +20,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *eachTotalLabel;
 @property (weak, nonatomic) IBOutlet UILabel *eachTipLabelField;
 @property (weak, nonatomic) IBOutlet UILabel *eachTotalLabelField;
+@property (weak, nonatomic) IBOutlet UILabel *groupText;
+@property (weak, nonatomic) IBOutlet UILabel *totalTipField;
+@property (weak, nonatomic) IBOutlet UILabel *totalTotalField;
+@property (weak, nonatomic) IBOutlet UILabel *billValueField;
 
 @end
 
@@ -42,19 +48,36 @@
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     int tipIndex = (int) [defaults integerForKey:@"defaultTipValue"];
     BOOL shouldSplitGroup = [defaults boolForKey:@"shouldSplitGroup"];
-    NSLog(@" %s", shouldSplitGroup ? "true" : "false");
+    self.totalTipField.layer.backgroundColor = [[UIColor orangeColor] CGColor];
+    self.totalTotalField.layer.backgroundColor = [[UIColor orangeColor] CGColor];
+    self.billValueField.layer.backgroundColor = [[UIColor orangeColor] CGColor];
+    UIColor *firstColor = Rgb2UIColor(214, 206, 195);
+    UIColor *secondColor = Rgb2UIColor(228, 221, 202);
+    
+    
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.colors = [NSArray arrayWithObjects:
+                       (id)firstColor.CGColor,
+                       (id)secondColor.CGColor,
+                       nil];
+    gradient.frame = self.view.bounds;
+    
+    // Add the gradient to the view
+    [self.view.layer insertSublayer:gradient atIndex:0];
     if(shouldSplitGroup) {
         self.peopleCountControl.alpha = 1;
         self.eachTipLabel.alpha = 1;
         self.eachTotalLabel.alpha = 1;
         self.eachTipLabelField.alpha = 1;
         self.eachTotalLabelField.alpha = 1;
+        self.groupText.alpha = 1;
     } else {
         self.peopleCountControl.alpha = 0;
         self.eachTipLabel.alpha = 0;
         self.eachTotalLabel.alpha = 0;
         self.eachTipLabelField.alpha = 0;
         self.eachTotalLabelField.alpha = 0;
+        self.groupText.alpha = 0;
     }
     [self.tipControl setSelectedSegmentIndex:tipIndex];
     [self updateValues];
